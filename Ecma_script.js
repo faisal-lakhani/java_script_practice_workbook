ba75b4075f2c61bf2cebc0d17f1f20ec68f3b880
faisal lakhani <faisallakhanitop10@gmail.com>
2025-12-06 12:37:27 -0800

    first commit

---
+++ Ecma_script.js
@@ -0,0 +1,136 @@
+//Java Scrip Practice workbook .
+
+//--Interacting with Users and Storing Text.--//
+
+//1. Alerts: Showing Messages to the User//
+//Excercises:
+
+//1. alert ("Hello world");
+//2. alert ("My name is faisal");
+//3. alert (100);
+//4. var name = ("Ali");
+//   alert (name);
+//5. var greet = ("Hello " + name);
+//   alert (greet);
+
+//2.Variales for Strings: Declaring and using Text Deta.
+
+//Excercises:
+
+//1. var userName = ("faisal");
+// console.log (userName);
+
+//2.var welcome = ("welcome to my house");
+// alert (welcome);
+
+
+//3.var firstName = ("faisal ");
+// var lastName = ("lakhani");
+// var fullName = (firstName + lastName);
+// // alert (fullName);
+// console.log (fullName);
+
+//4.var quote = ("i Love Java Script");
+// console.log (quote);
+
+//5.var city = ("karachi");
+//alert ("welcome to " + city);
+
+//--Working with Numbers and basic atithmetic --//
+
+//3.variables of Numbers: Storihg and using Numerical Data.
+//Excercisesa:
+
+//1.var age = (28);
+// console.log (age);
+
+//2.var num2 = (2);
+// var num5 = (5);
+// var total = ( num2 + num5);
+// console.log (total); 
+
+//3.var num2 = (2);
+// var num5 = (5);
+// var total = ( num2 - num5);
+// console.log (total);
+
+//4.var num2 = (2);
+// var num5 = (5);
+// var total = ( num2 * num5);
+// console.log (total);
+
+//5. var age = (20);
+// alert ("you are " + age + " years old");
+
+
+//4. Math Expressions: Performing arithmetic Operations.
+//Excersise.
+
+//1.var x = (10);
+// var y = (5);
+// var xy = ( x + y);
+// console.log (xy);
+ 
+//2.var x = (10);
+// var y = (5);
+// var xy = ( x - y);
+// alert (xy);
+
+//3. var x = (10);
+// var y = (5);
+// var xy = ( x * y);
+// console.log (xy);
+
+//4. var x = (10);
+// var y = (5);
+// var xy = ( x / y);
+// console.log (xy);
+
+//5. var x = (10);
+// var y = (5);
+// var xy = ( x % y);
+// console.log (xy);
+
+
+//-- Getting User input and Conditional Logic.
+//5. Prome: Getting input from the user.
+//Excercise;
+
+//1. var name = prompt("What is your name?");
+// alert ("Hello,"+ name);
+
+//2. var age = prompt("age?");
+// alert (age);
+
+//3.var number = prompt("number?");
+// var double = (number* 2)
+// alert (  "number "+ double );
+
+//4.var color = prompt("your favourite color?");
+// alert ("Wow! i like "+ color + " too!");
+
+//5.var city = prompt("your city name?");
+// alert ("you are from " + city);
+
+
+//6. If Statement and Comparison Operators: Controlling Flow
+//Excersise:
+
+//1.var age = prompt ("your age");
+// if (age <= 30){ alert ("you are still young ");}
+
+//2.var number = prompt("number");
+// if (number % 2 ==0){alert (" is a Even number");}
+
+// 3.var num1 = prompt("enter firt number");
+//  var num2 = prompt("enter second number");
+//  if (num1 > num2){alert (num1 + " is greator");}
+//  else {alert (num2 + " is greator");}
+
+
+
+
+
+
+
+

af7f3069f129fa98a8d7b0567da9beace940c860
faisal lakhani <faisallakhanitop10@gmail.com>
2025-12-06 13:04:35 -0800

    modified

--- Ecma_script.js
+++ Ecma_script.js
@@ -127,6 +127,28 @@
 //  if (num1 > num2){alert (num1 + " is greator");}
 //  else {alert (num2 + " is greator");}
 
+// 4.var password = (123);
+// var userPassword = prompt("enter your password");
+// if (userPassword == password){alert("Access granted");}
+// else{alert ("incorrect password");}
+
+//5.var num1 = ("10");
+//  var num2 = (10);
+//  console.log (num1 == num2);
+
+//   var num1 = ("10");
+//  var num2 = (10);
+//  console.log (num1 === num2);
+
+//   var num1 = ("10");
+//  var num2 = (10);
+//  console.log (num1 != num2);
+
+//   var num1 = ("10");
+//  var num2 = (10);
+//  console.log (num1 !== num2);
+
+
 
 
 
